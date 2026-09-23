373fcd3488dc03aa5fa7ac30a42d782065a44094
Luanna Davi <luanna.davi@escolar.ifrn.edu.br>
2026-09-23 17:08:48 -0300

    correção atvidade 9

--- js/app2.js
+++ js/app2.js
@@ -3,9 +3,15 @@ const cadastro = document.getElementById("cadastro");
 const nome = cadastro.nome;
 const nasc = cadastro.nasc;
 const whatsapp = cadastro.whatsapp;
+const Lista = document.getElementById("lista")
 cadastro.addEventListener("submit",function(e){
     e.preventDefault();
     let item = [nome.value,nasc.value,whatsapp.value];
     amigos.unshift(item);
     cadastro.reset();
+    exibirLista();
 });
+
+function exibirLista(){
+    
+}

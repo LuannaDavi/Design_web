42e17fbe799eb1987f73c033151f7439395bd3da
Luanna Davi <luanna.davi@escolar.ifrn.edu.br>
2026-09-23 17:56:52 -0300

    atividade 9

--- js/app2.js
+++ js/app2.js
@@ -13,5 +13,21 @@ cadastro.addEventListener("submit",function(e){
 });
 
 function exibirLista(){
-    
+    let itens = "";
+    for(let i = 0; i < amigos.length; i++){
+        let item = amigos[i];
+        let remover = `<button onclick="remover(${i})">Remover</button>`
+        let li = `<li>${item[0]}|${item[1]}|${item[2]}|${remover}</li>`;
+        itens = itens + li   
+    }
+    Lista.innerHTML = itens;
+}
+
+function remover(i){
+    let item = amigos[i];
+    let check = confirm (`Deseja realmente excluir ${item[0]}?`);
+    if (check == true){
+        amigos.splice(i,1);
+    }
+    exibirLista();
 }
